4edbf6ed9f02ad7f9ee7ab12f165641004ade1eb
Weronika <wkorbu16@student.aau.dl>
2017-10-31 14:59:18 +0100

    hihihih

---
+++ p3project/p3project/Profile.cpp
@@ -0,0 +1,9 @@
+#include "Profile.h"
+#include "stdafx.h"
+
+
+void newAccount:
+
+void Login:
+
+void ForgetPassword:
---
+++ p3project/p3project/Profile.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "stdafx.h"
+#include <opencv2\opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+class Profile {
+
+	float stats;
+	Mat avatar;
+	String username;
+	String password;
+	int Trophy[];
+
+public:
+	void newAccount();
+	void Login();
+	void ForgetPassword();
+
+
+};
