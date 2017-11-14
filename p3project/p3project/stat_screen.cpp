4c4ea16b0a89606b142538889233bb78073f7f01
Unknown <nolse16@student.aau.dk>
2017-11-14 13:34:29 +0100

    Revert "settings so opencv works"
    This reverts commit 6413548d05cdf240743fc6c02f5e40acd7478481.

--- p3project/p3project/stat_screen.cpp
+++ p3project/p3project/stat_screen.cpp
@@ -6,3 +6,18 @@
 
 using namespace std;
 using namespace cv;
+void stat_screen()
+{
+	Mat image;
+	image = imread(, CV_LOAD_IMAGE_COLOR);   // Read the file
+
+	if (!image.data)                              // Check for invalid input
+	{
+		cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+
+	namedWindow("Display window", WINDOW_AUTOSIZE);// Create a window for display.
+	imshow("Display window", image);                   // Show our image inside it.
+
+}
